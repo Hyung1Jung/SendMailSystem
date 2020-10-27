26ba5b8d03046f299f62042aa7d886ab3f1e9334
Hyung1Jung <junghyungile@gmail.com>
2020-10-27 20:11:48 +0900

    initial commit

---
+++ src/test/java/com/skhu/sof/hyungil/sendmail/SendmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhu.sof.hyungil.sendmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
