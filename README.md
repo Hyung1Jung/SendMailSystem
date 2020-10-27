cac7cd1d4456b3b9b5cf4e7763f70ee63c6a9376
Hyung1Jung <junghyungile@gmail.com>
2020-10-28 02:23:09 +0900

    email 발송

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Gmail SMTP Server를 이용하고 MailSender 인터페이스를 상속받은 Jav
 
 intellj + springboot + gradle + java11
 
+사진, 파일 포함한 메일 발송 완료
+![1](https://user-images.githubusercontent.com/43127088/97337899-2d717900-18c4-11eb-8287-8fdaa0200f90.PNG)
--- src/main/java/com/skhu/sof/hyungil/sendmail/service/MailService.java
+++ src/main/java/com/skhu/sof/hyungil/sendmail/service/MailService.java
@@ -15,20 +15,13 @@ public class MailService {
     public void mailSend(MailDto mailDto) {
         try {
             MailHandler mailHandler = new MailHandler(mailSender);
-
-            // 받는 사람
             mailHandler.setTo(mailDto.getAddress());
-            // 보내는 사람
             mailHandler.setFrom(MailService.FROM_ADDRESS);
-            // 제목
             mailHandler.setSubject(mailDto.getTitle());
-            // HTML Layout
             String htmlContent = "<p>" + mailDto.getMessage() +"<p> <img src='cid:sample-img'>";
             mailHandler.setText(htmlContent, true);
-            // 첨부 파일
-            mailHandler.setAttach("newTest.txt", "static/originTest.txt");
-            // 이미지 삽입
-            mailHandler.setInline("sample-img", "static/sample1.jpg");
+            mailHandler.setAttach("SendFileTest.txt", "static/SendFileTest.txt");
+            mailHandler.setInline("image1", "static/image1.jpg");
 
             mailHandler.send();
         }
