1f69d5c5726ace8521fb296e2fd91cc2b2be7c13
Hyung1Jung <junghyungile@gmail.com>
2020-10-28 00:35:07 +0900

    메일 전송 완료

---
+++ src/main/java/com/skhu/sof/hyungil/sendmail/Controller/MailController.java
@@ -0,0 +1,24 @@
+package com.skhu.sof.hyungil.sendmail.Controller;
+
+import com.skhu.sof.hyungil.sendmail.dto.MailDto;
+import com.skhu.sof.hyungil.sendmail.service.MailService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@AllArgsConstructor
+public class MailController {
+    private final MailService mailService;
+
+    @GetMapping("/mail")
+    public String dispMail() {
+        return "mail";
+    }
+
+    @PostMapping("/mail")
+    public void execMail(MailDto mailDto) {
+        mailService.mailSend(mailDto);
+    }
+}
