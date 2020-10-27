be8733421bd86ce3ae9bbf258be3894b71a5cc32
Hyung1Jung <junghyungile@gmail.com>
2020-10-28 00:34:43 +0900

    dto

---
+++ src/main/java/com/skhu/sof/hyungil/sendmail/dto/MailDto.java
@@ -0,0 +1,14 @@
+package com.skhu.sof.hyungil.sendmail.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MailDto {
+    private String address;
+    private String title;
+    private String message;
+}
